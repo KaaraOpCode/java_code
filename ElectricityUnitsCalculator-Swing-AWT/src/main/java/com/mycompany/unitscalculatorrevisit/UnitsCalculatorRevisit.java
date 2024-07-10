6acd560ca050b605463705cf60bdd168bf1fcabd
Tlokotsi Potloane <146142924+KaaraOpCode@users.noreply.github.com>
2024-07-10 15:38:15 +0100

    ElectricityUnitsCalculator - swing & awt

---
+++ ElectricityUnitsCalculator-Swing-AWT/src/main/java/com/mycompany/unitscalculatorrevisit/UnitsCalculatorRevisit.java
@@ -0,0 +1,66 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.unitscalculatorrevisit;
+
+import java.awt.HeadlessException;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.*;
+
+/**
+ *
+ * @author sebabatso.metla
+ */
+public class UnitsCalculatorRevisit  extends JFrame{
+    
+    
+        JTextField txtNumOfUnits = new JTextField("Enter number of units");
+        JTextField txtPricePerUnit = new JTextField("Enter price per unit");
+        JTextField txtTotalBill = new JTextField("Total");
+        
+        JButton btnCalculate = new JButton("Calculate");  
+       
+    public UnitsCalculatorRevisit(){
+        
+        JPanel panel = new JPanel();
+        panel.add(txtNumOfUnits);
+        panel.add(txtPricePerUnit);
+        panel.add(txtTotalBill);
+        panel.add(btnCalculate);
+        
+        add(panel);  
+        
+        
+        btnCalculate.addActionListener(new bntCalculateListner());
+    }    
+    
+
+    public static void main(String[] args) {
+        
+        
+        UnitsCalculatorRevisit calculator = new UnitsCalculatorRevisit();
+        calculator.setTitle("Calculator");
+        calculator.pack();
+        calculator.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        calculator.setVisible(true);
+        
+    }    
+    
+    public class bntCalculateListner implements ActionListener {
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        
+        double numOfUnits =  Double.parseDouble( txtNumOfUnits.getText());
+        double pricePerUnit =  Double.parseDouble( txtPricePerUnit.getText());
+        
+        double totalBill = numOfUnits * pricePerUnit;
+        
+        txtTotalBill.setText(Double.toString(totalBill));
+                
+        }
+}
+
+}
